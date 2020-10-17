f573165c43a5aa42733b409e7aabec7bd0b0f638
Anisha Kabir <ahsina.ribak@gmail.com>
2020-10-16 22:49:44 -0700

    added retweet functionality

--- Twitter/TweetCell.swift
+++ Twitter/TweetCell.swift
@@ -47,7 +47,22 @@ class TweetCell: UITableViewCell {
     }
     
     @IBAction func retweet(_ sender: Any) {
-        
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: { (Error) in
+            print("error")
+        })
+    }
+    
+    func setRetweeted(_ isRetweeted:Bool){
+        if(isRetweeted){
+            retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        }
+        else{
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true
+        }
     }
     
     override func awakeFromNib() {
